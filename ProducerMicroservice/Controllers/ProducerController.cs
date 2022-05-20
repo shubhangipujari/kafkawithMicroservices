e1d541862ebf143d0cc59a81f390ab2bb8b48fd0
unknown <cogdotnet08@iiht.tech>
2022-05-21 00:05:18 +0530

    save

---
+++ ProducerMicroservice/Controllers/ProducerController.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProducerMicroservice.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProducerMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+
+        public ProducerController(ProducerConfig config)
+        {
+            _config = config;
+        }
+        [HttpPost("send")]
+        public async Task<ActionResult> Get(string topic, [FromBody] Employee employee)
+        {
+            string employeeData = JsonConvert.SerializeObject(employee);
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = employeeData });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+
+        }
+    }
+}
